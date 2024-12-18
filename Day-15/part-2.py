1c012bdc3e63915d5c58724e26d76175156699e4
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2024-12-18 17:40:09 +1100

    Completed Day-15

--- Day-15/part-2.py
+++ Day-15/part-2.py
@@ -2,7 +2,9 @@ import sys
 sys.setrecursionlimit(1000000000)
 grid = []
 moves = ""
-with open("input.txt") as f:
+
+filename = sys.argv[1]
+with open(filename) as f:
     ok = True 
     for line in f:
         if line.strip() == "": 
@@ -16,53 +18,60 @@ def convert(a):
     elif a == '#': return '##'
     elif a == 'O': return '[]'
     return '@.'
-def simulate(pos, dir): 
+
+def check(pos, dir): 
     (dx, dy) = dir 
     (i, j) = pos    
-
-
     if grid[i][j] == '#': return False
     elif grid[i][j] == '@':
-        ok = simulate((i + dx, j + dy), dir)
-        if ok: 
-            grid[i][j], grid[i + dx][j + dy] = grid[i + dx][j + dy], grid[i][j]
-            global robot 
-            robot = (i + dx, j + dy)
-
+        return check((i + dx, j + dy), dir)
     elif grid[i][j] == ']':
         if dir == (1, 0) or dir == (-1, 0):
-            ok = True 
-            ok &= simulate((i + dx, j + dy), dir)
-            ok &= simulate((i + dx, (j - 1) + dy), dir)
-            if ok:
-                grid[i][j], grid[i + dx][j + dy] = grid[i + dx][j + dy], grid[i][j]
-                grid[i][j - 1], grid[i + dx][(j - 1) + dy] = grid[i + dx][(j - 1) + dy], grid[i][j - 1]
-            return ok
+            ok1 = check((i + dx, j + dy), dir)
+            ok2 = check((i + dx, (j - 1) + dy), dir)
+            return ok1 and ok2
         else:
-            ok = simulate((i, j - 2), dir) 
-            if ok:
-                grid[i][j - 1], grid[i][j - 2] = grid[i][j - 2], grid[i][j - 1]
-                grid[i][j], grid[i][j - 1] = grid[i][j - 1], grid[i][j] 
-            return ok
+            return check((i, j - 2), dir) 
     elif grid[i][j] == '[': 
         if dir == (1, 0) or dir == (-1, 0):
-            ok = True 
-            ok &= simulate((i + dx, j + dy), dir)
-            ok &= simulate((i + dx, (j + 1) + dy), dir)
-            if ok:
-                grid[i][j], grid[i + dx][j + dy] = grid[i + dx][j + dy], grid[i][j]
-                grid[i][j + 1], grid[i + dx][(j + 1) + dy] = grid[i + dx][(j + 1) + dy], grid[i][j + 1]
-            return ok
+            ok1 = check((i + dx, j + dy), dir)
+            ok2 = check((i + dx, (j + 1) + dy), dir)
+            return ok1 and ok2
         else:
-            ok = simulate((i, j + 2), dir) 
-            if ok:
-                grid[i][j + 1], grid[i][j + 2] = grid[i][j + 2], grid[i][j + 1]
-                grid[i][j], grid[i][j + 1] = grid[i][j + 1], grid[i][j] 
-            return ok
-    else: 
-        return True 
-    return False
+            return check((i, j + 2), dir) 
+    return True 
         
+def simulate(pos, dir):
+    (dx, dy) = dir 
+    (i, j) = pos    
+    if grid[i][j] == '#': return
+    elif grid[i][j] == '@':
+        simulate((i + dx, j + dy), dir)
+        grid[i][j], grid[i + dx][j + dy] = grid[i + dx][j + dy], grid[i][j]
+        global robot
+        robot = (i + dx, j + dy)
+    elif grid[i][j] == ']':
+        if dir == (1, 0) or dir == (-1, 0):
+            simulate((i + dx, j + dy), dir)
+            simulate((i + dx, (j - 1) + dy), dir)
+            grid[i][j], grid[i + dx][j] = grid[i + dx][j], grid[i][j]
+            grid[i][j - 1], grid[i + dx][j - 1] = grid[i + dx][j - 1], grid[i][j - 1]
+        else:
+            simulate((i, j - 2), dir) 
+            grid[i][j - 1], grid[i][j - 2] = grid[i][j - 2], grid[i][j - 1]
+            grid[i][j - 1], grid[i][j] = grid[i][j], grid[i][j - 1]
+    elif grid[i][j] == '[': 
+        if dir == (1, 0) or dir == (-1, 0):
+            simulate((i + dx, j + dy), dir)
+            simulate((i + dx, (j + 1) + dy), dir)
+            grid[i][j], grid[i + dx][j] = grid[i + dx][j], grid[i][j]
+            grid[i][j + 1], grid[i + dx][j + 1] = grid[i + dx][j + 1], grid[i][j + 1]
+        else:
+            simulate((i, j + 2), dir) 
+            grid[i][j + 1], grid[i][j + 2] = grid[i][j + 2], grid[i][j + 1]
+            grid[i][j + 1], grid[i][j] = grid[i][j], grid[i][j + 1]
+    return
+ 
 n, m = len(grid), len(grid[0])
 
 new_grid = []
@@ -81,18 +90,24 @@ for i in range(n):
             robot = (i, j)
 
 for move in moves: 
-    if move == '^': simulate(robot, (-1, 0))
-    elif move == '<': simulate(robot, (0, -1))
-    elif move == '>': simulate(robot, (0, 1))
-    elif move == 'v': simulate(robot, (1, 0))
+    if move == '^': 
+        if check(robot, (-1, 0)):
+            simulate(robot, (-1, 0))
+    elif move == '<':
+        if check(robot, (0, -1)):
+            simulate(robot, (0, -1))
+    elif move == '>': 
+        if check(robot, (0, 1)):
+            simulate(robot, (0, 1)) 
+    elif move == 'v': 
+        if check(robot, (1, 0)):
+            simulate(robot, (1, 0))
     else: pass
-
 ans = 0 
 
 for i in range(n):
     for j in range(m):
         if grid[i][j] == '[': ans += 100 * i + j
-for g in grid: print(g)
 print(ans)
     
 
