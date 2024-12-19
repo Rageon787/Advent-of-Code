dab8cab850fd72594a356544a46c39d5086893bb
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2024-12-19 16:41:17 +1100

    Completed day 19

---
+++ Day-19/part-2.py
@@ -0,0 +1,22 @@
+import sys 
+ 
+filename = sys.argv[1]
+with open(filename) as f: 
+    raw_input = f.read().split('\n')
+    patterns = raw_input[0].strip().split(', ')
+    designs = [design.strip('\n') for design in raw_input[2:-1]]
+
+ans = 0
+for design in designs:
+    n = len(design) 
+    dp = [0] * (n + 1)
+    for i in range(n): 
+        for pattern in patterns: 
+            left = i - len(pattern) 
+            if left < -1: continue
+            sub_design = design[left + 1:i + 1]
+            if sub_design == pattern:
+                if left == -1: dp[i] += 1
+                else: dp[i] += dp[left]
+    if dp[n - 1] > 0: ans += dp[n - 1]
+print(ans)
